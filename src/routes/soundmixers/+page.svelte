c430cb9f8afd447afdf537b51d522d65a464aca0
Adam <chillcoders@gmail.com>
2022-11-09 17:56:44 -0500

    flex-main-page

--- src/routes/soundmixers/+page.svelte
+++ src/routes/soundmixers/+page.svelte
@@ -1,37 +1,41 @@
+<script>
+	import Gif from '../../lib/gif.svelte';
+</script>
+
 <svelte:head>
 	<title>Sound Mixers</title>
 </svelte:head>
+
 <h1 class="text-4xl text-center my-8 uppercase">Sound Mixers</h1>
 
-<img
-	src="https://res.cloudinary.com/adamaslan/image/upload/v1653434875/nycsoundguy/adam_eh3ekw.jpg"
-	alt="adam"
-/>
-<br />
-
-<p>
-	Hit us up to work with the following top notch soundmixers:
+<div class="p-8 max-w-6xl mx-auto flex flex-col justify-center align-middle">
+	<Gif />
 	<br />
 
-	<br />
-	<a href="/Billpierce">Bill Pierce </a>
-	<br />
-	<br />
-	<a href="/MichaelMoote">Michael Moote </a>
-	<br />
-	<br />
-	<a href="/AllistairJohnson">Allistair Johnson </a>
-	<br />
-	<br />
-	<a href="/BrettAinslie">Brett Ainslie </a>
-	<br />
-	<br />
-	<a href="/Adamaslan">Adam Aslan </a>
-	<br />
-	<br />
-	<a href="/JoePfeil">Joe Pfeil </a>
-	<br />
-	<br />
-	<a href="DrewJoy">Drew Joy </a>
-	<br />
-</p>
+	<p>
+		Hit us up to work with the following top notch soundmixers:
+		<br />
+
+		<br />
+		<a href="/Billpierce">Bill Pierce </a>
+		<br />
+		<br />
+		<a href="/MichaelMoote">Michael Moote </a>
+		<br />
+		<br />
+		<a href="/AllistairJohnson">Allistair Johnson </a>
+		<br />
+		<br />
+		<a href="/BrettAinslie">Brett Ainslie </a>
+		<br />
+		<br />
+		<a href="/Adamaslan">Adam Aslan </a>
+		<br />
+		<br />
+		<a href="/JoePfeil">Joe Pfeil </a>
+		<br />
+		<br />
+		<a href="DrewJoy">Drew Joy </a>
+		<br />
+	</p>
+</div>
